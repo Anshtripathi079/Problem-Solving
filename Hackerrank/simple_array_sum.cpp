c4419aab06ee699c6fe3b3611e7bbbf913c985db
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:48:54 +0530

    Create simple_array_sum.cpp

---
+++ Hackerrank/simple_array_sum.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int i,n,*arr,sum=0;
+  scanf("%d",&n);
+  arr = (int *)malloc(n*sizeof(int));
+  for(i=0;i<n;i++){
+  scanf("%d",&arr[i]); 
+}
+     for(i=0;i<n;i++){
+         sum += arr[i];
+     }
+     printf("%d ",sum);
+}

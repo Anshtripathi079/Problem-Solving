747708af3ab24b7d2d69ad0927c18e760e6ff1b9
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-01-01 10:31:11 +0530

    Create prime_number.cpp

---
+++ GFG/prime_number.cpp
@@ -0,0 +1,34 @@
+class Solution{
+public:
+   int isPrime(int N){
+
+        // code here
+
+        if(N < 2)
+
+        {
+
+            return 0;
+
+        }
+
+        for(int i = 2;i*i<=N;i++)
+
+        {
+
+            if(N % i == 0)
+
+            {
+
+                return 0;
+
+            }
+
+        }
+
+        return 1;
+
+        
+
+    }
+};
